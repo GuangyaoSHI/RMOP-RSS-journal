37b58c74cccc334133ec96a9546b10da5c03e2ee
GuangyaoSHI <gyshi@terpmail.umd.edu>
2021-01-22 23:49:22 -0500

    tomorrow task

--- brute_force_baseline.py
+++ brute_force_baseline.py
@@ -9,16 +9,16 @@ import networkx as nx
 from gamestate import *
 from utilities import *
 import copy
-import sys 
+import sys
 import time
 
+# change the game turn tomorrow and retest
+starts = {0: [(0, 1), 0], 1: [(0, 1), 0], 2: [(0, 0), 0], 3: [(0, 0), 0]}
 
-starts = {0:[(0,1), 0], 1:[(0,1), 0], 2:[(0,0), 0], 3:[(0,0), 0]}
-
-#start a game
+# start a game
 board = setup_game()
 game = GameState(board, starts)
-#return all feasible paths
+# return all feasible paths
 paths = all_paths(starts, game)
 attacks = all_attacks(list(starts.keys()), game)
 
@@ -26,14 +26,14 @@ reward_max = 0
 path_attack = ()
 
 for choice in product(*list(paths.values())):
-    #print('choice is {}'.format(choice))
+    # print('choice is {}'.format(choice))
     start = time.time()
     reward_min = sys.maxsize
-    path_attack_min = () 
+    path_attack_min = ()
     for attack in attacks:
-        #print('attack is {}'.format(attack))
+        # print('attack is {}'.format(attack))
         reward = compute_reward(game, choice, attack)
-        #print('reward is {}'.format(reward))
+        # print('reward is {}'.format(reward))
         if reward < reward_min:
             reward_min = reward
             path_attack_min = (choice, attack)
@@ -41,11 +41,11 @@ for choice in product(*list(paths.values())):
         reward_max = reward_min
         path_attack = path_attack_min
     end = time.time()
-   # print('one choice time spent is {}'.format(end-start))
+# print('one choice time spent is {}'.format(end-start))
 
 print('worst-case reward is {}'.format(reward_max))
-path_attack[0] 
-#84            
-#np.array([94,88,86,88,88])
+path_attack[0]
+# 84
+# np.array([94,88,86,88,88])
 
-print('test github')                           
+print('test github')
